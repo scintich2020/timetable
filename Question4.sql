1d61fc5b76e310a773e306f554b7f7d657a38d50
scintich2020 <73691344+scintich2020@users.noreply.github.com>
2021-02-06 11:14:14 +0100

    Update Question4.sql

--- Question4.sql
+++ Question4.sql
@@ -1,9 +1,6 @@
 --alter table Etudiant add password varchar(50);
-
 update Etudiant set password = ora_hash(matricule) where matricule=&valeur;
 --SELECT courCodeCours FROM Etudiantdeclasse WHERE etudiantMatricule=&matricule;
-
 -- alter table enseignants add password varchar(50);
-
 -- update enseignants set password = ora_hash(matricule) where matricule = valeur;
 
